4517f7ff59cb951f400351a2a5d613c97fc8ee92
Jay Wong <junteng.wong01@gmail.com>
2024-06-15 18:17:32 +0800

    Added a result page (design only).Changed 10 files, and tracked 2 files

--- app/src/main/java/com/example/assignmentmobileapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/assignmentmobileapp/QuizQuestionsActivity.kt
@@ -17,6 +17,7 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition: Int = 1
     private var mQuestionsList: ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
+    private var mCorrectAnswers: Int = 0
 
     private lateinit var tvOptionOne: TextView
     private lateinit var tvOptionTwo: TextView
@@ -129,6 +130,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                     val question = mQuestionsList?.get(mCurrentPosition - 1)
                     if (question!!.correctAnswer != mSelectedOptionPosition) {
                         answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }else{
+                        mCorrectAnswers++
                     }
                     answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
                     if (mCurrentPosition == mQuestionsList!!.size) {
